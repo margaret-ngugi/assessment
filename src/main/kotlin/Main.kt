25684aeb83a683b6690887d43d17f85bc4ca7ca5
Mitingi Joy <mitingijoychesang@gmail.com>
2023-03-09 08:50:47 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+fun main() {
+//Q1.
+    printlin("num1:mary","num2:wairimu)
+//Q2.
+    println(numbers())
+        //Q3.
+    println(person)
+
+
+}
+//Q1.
+fun animals(an1:String,an2:String):String{
+    an1 == an2
+    return longer String
+}
+//Q2.
+fun numbers(){
+    var numbers = arrayOf(6,8,10)
+    numbers = numbers.sortedArray()
+    numbers = numbers.sorted()
+
+}
+//Q3.
+fun person(x:String){
+    for (x.split)
+}
+//Q4.
+fun sentence(){
+    var students = arrayOf("name1:String","name2:String")
+    students = "name1 + name2"
+}
+//Q5.
+class calculator(var:colour,var:on,var:off){
+    fun addition(num1 +num2)
+}
+
+fun addition(){
+    println(num1  + num2)
+}
+fun subtraction(){
+    printlin(num1 - num2)
+}
+fun division(){
+    println(num1 / num2)
+}
+fun multiply(){
+    println(num1 * num2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
